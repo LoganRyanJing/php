5f467416c4960ad00038e316a51dce01c492fd1e
logan <425068896@qq.com>
2017-03-15 15:13:33 +0800

    新增店铺类

--- app/exam/cls/shop.cls.php
+++ app/exam/cls/shop.cls.php
@@ -14,82 +14,82 @@ class shop_exam
 		$this->ev = $this->G->make('ev');
 	}
 
-	//获取地名列表
+	//获取店铺列表
 	//参数：无
-	//返回值：地区列表
-	public function getAreaList()
+	//返回值：店铺列表
+	public function getShopList()
 	{
-		$data = array(false,'area','1',false,'areaid ASC');
+		$data = array(false,'shop','1',false,'shopid ASC');
 		$sql = $this->pdosql->makeSelect($data);
-		return $this->db->fetchAll($sql,'areaid');
+		return $this->db->fetchAll($sql,'shopid');
 	}
 
-	//按页获取地名列表
+	//按页获取店铺列表
 	//参数：无
-	//返回值：地区列表
-	public function getAreaListByPage($page,$number = 20)
+	//返回值：店铺列表
+	public function getShopListByPage($page,$number = 20)
 	{
 		$data = array(
 			'select' => false,
-			'table' => 'area',
+			'table' => 'shop',
 			'query' => 1,
-			'index' => 'areaid'
+			'index' => 'shopid'
 		);
 		$r = $this->db->listElements($page,$number,$data);
 		return $r;
 	}
 
-	//根据地名查询
-	//参数：地名字符串
-	//返回值：该地名信息数组
-	public function getAreaByName($area)
+	//根据店名查询
+	//参数：店名字符串
+	//返回值：该店名信息数组
+	public function getShopByName($shop)
 	{
-		$data = array(false,'area',array(array("AND","area = :area",'area',$area)),false,false,false);
+		$data = array(false,'shop',array(array("AND","shop = :shop",'shop',$shop)),false,false,false);
 		$sql = $this->pdosql->makeSelect($data);
 		return $this->db->fetch($sql);
 	}
 
-	//根据ID获取地名信息
-	//参数：地名ID
-	//返回值：该地名信息数组
-	public function getAreaById($areaid)
+	//根据ID获取店名信息
+	//参数：店名ID
+	//返回值：该店名信息数组
+	public function getShopById($shopid)
 	{
-		$data = array(false,'area',array(array("AND","areaid = :areaid",'areaid',$areaid)));
+		$data = array(false,'shop',array(array("AND","shopid = :shopid",'shopid',$shopid)));
 		$sql = $this->pdosql->makeSelect($data);
 		return $this->db->fetch($sql);
 	}
 
-	//修改地名信息
-	//参数：地名ID,要修改的信息
+	//修改店铺信息
+	//参数：店铺ID,要修改的信息
 	//返回值：true
-	public function modifyArea($areaid,$args)
+	public function modifyShop($shopid,$args)
 	{
-		$data = array('area',$args,array(array("AND","areaid = :areaid",'areaid',$areaid)));
+		$data = array('shop',$args,array(array("AND","shopid = :shopid",'shopid',$shopid)));
 		$sql = $this->pdosql->makeUpdate($data);
 		$this->db->exec($sql);
 		return true;
 	}
 
-	//增加地区
-	//参数：要添加的地区的信息数组
-	//返回值：地名ID
-	public function addArea($args)
+	//增加店铺
+	//参数：要添加的店铺的信息数组
+	//返回值：店铺ID
+	public function addShop($args)
 	{
-		$data = array(false,'area',array(array("AND","areaid = :areaid",'areaid',$args['areacode'])));
+		$data = array(false,'shop',array(array("AND","shopid = :shopid",'shopid',$args['areacode'])));
 		$sql = $this->pdosql->makeSelect($data);
 		if($this->db->fetch($sql))return false;
-		$data = array('area',$args);
+		$data = array('shop',$args);
 		$sql = $this->pdosql->makeInsert($data);
 		$this->db->exec($sql);
 		return $this->db->lastInsertId();
 	}
 
-	//删除地区
-	//参数：地名ID
+	//删除店铺
+	//参数：店铺ID
 	//返回值：受影响的记录数
-	public function delArea($id)
+	public function delShop($id)
 	{
-		$data = array('area',array(array("AND","areaid = :areaid",'areaid',$id)));
+		$data = array('area',array(array("AND","shopid = :shopid",'shopid',$id)));
 		$sql = $this->pdosql->makeDelete($data);
 		$this->db->exec($sql);
 		return $this->db->affectedRows();
